bd932dd992580a80f42815dabf38ed5a16ae0acd
MadhujyaGogoi <70769095+MadhujyaGogoi@users.noreply.github.com>
2021-02-02 20:00:53 +0530

    Final year project update splash screen

--- app/src/main/java/com/example/mregency/MainActivity.java
+++ app/src/main/java/com/example/mregency/MainActivity.java
@@ -2,16 +2,33 @@ package com.example.mregency;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.hardware.biometrics.BiometricManager;
 import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=4000;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent Log_In = new Intent(MainActivity.this , LogInForm.class);
+                startActivity(Log_In);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+
 
 
     }
